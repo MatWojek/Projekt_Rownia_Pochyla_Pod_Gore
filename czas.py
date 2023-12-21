28568e5f813a39b061b9cafb9ff2952c47fff7c1
MaciejSzczepkowski <119876278+MaciejSzczepkowski@users.noreply.github.com>
2023-12-21 21:18:53 +0100

    Update czas.py

--- czas.py
+++ czas.py
@@ -15,7 +15,7 @@ print("kat rowna sie: " + str(kat))
 # konwersja kąta z stopni na radiany
 kat_w_radianach = math.radians(kat)
 
-#obliczenia sily grawitacyjnej na rowni pochylej
+#obliczenia czasu, w ciele ciala wjedzie po rowniej pochylej
 czas = - (v0 + math.sqrt(v0**2 + 2 * G * (u * math.cos(kat_w_radianach) - math.sin(kat_w_radianach)) * s)) / (G * (math.sin(kat_w_radianach) - u * math.cos(kat_w_radianach)))
 #wynik
 print(f"czas rowna sie: {czas:.2f} sekund")
